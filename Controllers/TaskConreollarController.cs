61875ed9548ce15f51db41941ad1e53d92c16ce4
sajja <sajja@DESKTOP-D5LCND8>
2025-08-16 12:50:09 +0600

    Add project files.

---
+++ Controllers/TaskConreollarController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tempTaskmanage.Model;
+
+namespace tempTaskmanage.Controllers
+{
+    [Route("api/tasks")]
+    [ApiController]
+    public class TaskConreollarController : ControllerBase
+    {
+        private List<TaskItem> _tasks = new List<TaskItem>()
+        {
+
+           new TaskItem {Id=1, Title="Task1", Description="Welcome to our "},
+           new TaskItem {Id=2, Title="Task2", Description="Welcome to our "},
+};
+   
+
+        //GET
+        [HttpGet("getall")]
+        public ActionResult GetTask()
+        {
+            if (_tasks.Count == 0)
+            {
+                return NotFound("No task found");
+            }
+            return Ok(_tasks);
+        }
+        //POST
+        [HttpPost("create")]
+        public ActionResult CreateTask([FromBody] TaskItem task)
+        { 
+            if (task== null || string.IsNullOrEmpty(task.Title))
+            {
+                return BadRequest("Invalid");
+            }
+            return Ok(_tasks);
+        }  
+    }        
+}
---
+++ Model/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace tempTaskmanage.Model
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public  string Title { get; set; }
+        public  string Description { get; set; }
+        public  DateTime CreateAt { get; set; }
+        public DateTime? CompleteAt { get; set; }
+    }
+}
